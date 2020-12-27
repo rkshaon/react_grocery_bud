c0cd1772aefa2e6fe7e70ba13d3134a81046bb7c
Rezaul Karim Shaon <rkshaon.ist@gmail.com>
2020-12-27 23:34:38 +0600

    list

--- src/App.js
+++ src/App.js
@@ -3,7 +3,47 @@ import List from './List'
 import Alert from './Alert'
 
 function App() {
-  return <h2>grocery bud setup</h2>
+  const [name, setName] = useState('');
+  const [list, setList] = useState([]);
+  const [isEditing, setIsEditing] = useState(false);
+  const [editID, setEditID] = useState(null);
+  const [alert, setAlert] = useState({show:false, msg:'', type:''});
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log('submit');
+    if (!name) {
+      // display alert
+    } else if (name && isEditing) {
+      // editing
+    } else {
+      // show alert
+      const newItem = {id: new Date().getTime().toString(), title: name};
+      setList([...list, newItem]);
+      setName('');
+    }
+  }
+
+  return(
+    <section className="section-center">
+      <form className="grocery-form" onSubmit={handleSubmit}>
+        {alert.show && <Alert />}
+        <h3>grocery bud</h3>
+        <div className="form-control">
+          <input tpe="text" className="grocery" placeholder="e.g. eggs" onChange={(e)=>{setName(e.target.value)}} />
+          <button type="submit" className="submit-btn">
+            {isEditing ? 'edit' : 'add'}
+          </button>
+        </div>
+      </form>
+      {list.length > 0 &&
+        <div className="grocery-container">
+          <List items={list} />
+          <button className="clear-btn">clear all items</button>
+        </div>
+      }
+    </section>
+  );
 }
 
 export default App;
--- src/List.js
+++ src/List.js
@@ -1,7 +1,22 @@
 import React from 'react'
 import { FaEdit, FaTrash } from 'react-icons/fa'
-const List = () => {
-  return <h2>list component</h2>
+const List = ({items}) => {
+  return(
+    <div className="grocery-list">
+      {items.map((item)=>{
+        const {id, title} = item;
+        return(
+          <article key={id} className="grocery-item">
+            <p className="title">{title}</p>
+            <div className="btn-container">
+              <button type="button" className="edit-btn"><FaEdit /></button>
+              <button type="button" className="delete-btn"><FaTrash /></button>
+            </div>
+          </article>
+        );
+      })}
+    </div>
+  );
 }
 
 export default List
